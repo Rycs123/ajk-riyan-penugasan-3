76e5e00e14877a574e96a0f94bf76b7628fc52ea
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-05-16 04:13:47 +0700

    setelah debug kesekian kali build aja dulu

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 # Ensure the app key is set
 php artisan key:generate
 
+echo "Waiting for MySQL to be ready..."
+until mysql -h mysql -u root -proot -e "SHOW DATABASES;" > /dev/null 2>&1; do
+    sleep 3
+done
+
 # Run migrations and seed the database
 php artisan migrate
 php artisan db:seed
