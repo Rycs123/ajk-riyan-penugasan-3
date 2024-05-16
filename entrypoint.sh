73046138d7a0956f32fd133f92d515105db1f3f7
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-05-16 15:04:59 +0700

    test build and deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+php artisan cache:clear
+php artisan route:clear
 php artisan config:clear
-
+php artisan view:clear
 php artisan key:generate
+
+echo "Waiting for MySQL to be ready..."
+until mysql -h mysql -u root -proot -e "SHOW DATABASES;" > /dev/null 2>&1; do
+    sleep 10
+done
 
 php artisan migrate
 
