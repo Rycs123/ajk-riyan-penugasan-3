8ad74b679ae66da867e7158ed2153fc1cec033e2
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-05-17 20:53:26 +0700

    test depl

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import viteReact from "@vitejs/plugin-react";
+import viteReact from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     viteReact(),
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.tsx"],
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
       refresh: true,
     }),
   ],
   resolve: {
     alias: {
-      "@": "/resources/js",
+      '@': '/resources/js',
     },
   },
 });
